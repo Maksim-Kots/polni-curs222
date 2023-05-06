4bc9c1ee99443cce5c23d0099bafb4484c4da941
Maksim-Kots <112854062+Maksim-Kots@users.noreply.github.com>
2023-05-06 08:37:26 +0300

    Add files via upload

---
+++ etherium222/fafs.js
@@ -0,0 +1,34 @@
+<div class="tradingview-container">
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js" async>
+  {
+  "symbols": [
+    [
+      "FOREXCOM:ETHUSD|1M"
+    ]
+  ],
+  "chartOnly": false,
+  "width": "750",
+  "height": "550",
+  "locale": "ru",
+  "colorTheme": "light",
+  "autosize": false,
+  "showVolume": true,
+  "showMA": false,
+  "hideDateRanges": false,
+  "hideMarketStatus": false,
+  "hideSymbolLogo": true,
+  "scalePosition": "right",
+  "scaleMode": "Normal",
+  "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+  "fontSize": "10",
+  "noTimeScale": false,
+  "valuesTracking": "1",
+  "changeMode": "price-and-percent",
+  "chartType": "line",
+  "volumeUpColor": "rgba(136, 14, 79, 1)",
+  "volumeDownColor": "rgba(0, 96, 100, 1)",
+  "lineWidth": 3,
+  "lineType": 0
+}
+  </script>
+</div>
